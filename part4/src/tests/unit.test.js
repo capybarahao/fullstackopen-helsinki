cae8824609772cf94ad76b2246631a2926e57318
Qiyue Hao <yue.hao031@gmail.com>
2026-06-01 07:31:26 +0800

    test: 4.4

--- part4/src/tests/unit.test.js
+++ part4/src/tests/unit.test.js
@@ -8,3 +8,69 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   assert.strictEqual(result, 1)
 })
+
+describe('total likes', () => {
+  const listWithOneBlog = [
+    {
+      id: 1,
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+    },
+  ]
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 5)
+  })
+
+  const listWithBlogs = [
+    {
+      id: 2,
+      title: 'React patterns',
+      author: 'Michael Chan',
+      url: 'https://reactpatterns.com/',
+      likes: 7,
+    },
+    {
+      id: 3,
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+    },
+    {
+      id: 4,
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: 12,
+    },
+    {
+      id: 5,
+      title: 'First class tests',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+      likes: 10,
+    },
+    {
+      id: 6,
+      title: 'TDD harms architecture',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+      likes: 0,
+    },
+    {
+      id: 7,
+      title: 'Type wars',
+      author: 'Robert C. Martin',
+      url: 'https://blog.codinghorror.com/type-wars/',
+      likes: 2,
+    },
+  ]
+  test('when list has multiple blogs, sum the likes of that', () => {
+    const result = listHelper.totalLikes(listWithBlogs)
+    assert.strictEqual(result, 36)
+  })
+})
