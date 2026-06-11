014bc9875f6f0981195fd9a56f534c318816e3c2
Qiyue Hao <yue.hao031@gmail.com>
2026-06-11 18:13:10 +0800

    refactor: ditch try catch block. test pass

--- part4/src/controllers/blogsControllers.js
+++ part4/src/controllers/blogsControllers.js
@@ -1,30 +1,22 @@
 const Blog = require('../models/blog')
 
-const getAll = async (_request, response, next) => {
-  try {
-    const blogs = await Blog.getAll()
-    response.json(blogs)
-  } catch (error) {
-    next(error)
-  }
+const getAll = async (_request, response, _next) => {
+  const blogs = await Blog.getAll()
+  response.json(blogs)
 }
 
-const create = async (request, response, next) => {
-  try {
-    const { title, author, url, likes } = request.body
+const create = async (request, response, _next) => {
+  const { title, author, url, likes } = request.body
 
-    if (!title) {
-      return response.status(400).json({ error: 'title missing' })
-    }
-    if (!url) {
-      return response.status(400).json({ error: 'url missing' })
-    }
-
-    const blog = await Blog.create(title, author, url, likes)
-    response.status(201).json(blog)
-  } catch (error) {
-    next(error)
+  if (!title) {
+    return response.status(400).json({ error: 'title missing' })
   }
+  if (!url) {
+    return response.status(400).json({ error: 'url missing' })
+  }
+
+  const blog = await Blog.create(title, author, url, likes)
+  response.status(201).json(blog)
 }
 
 module.exports = { getAll, create }
--- part4/src/utils/logger.js
+++ part4/src/utils/logger.js
@@ -1,9 +1,14 @@
 const info = (...params) => {
-  console.log(...params)
+  if (process.env.NODE_ENV !== 'test') {
+    // so that it does not print to the console in test mode
+    console.log(...params)
+  }
 }
 
 const error = (...params) => {
-  console.error(...params)
+  if (process.env.NODE_ENV !== 'test') {
+    console.error(...params)
+  }
 }
 
 module.exports = { info, error }
